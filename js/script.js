d99cc2ce3bb6c3cadec9acbd460038a7fb1cac0c
Anthony Villegas <villegasanthony87@yahoo.com>
2021-10-04 23:00:47 -0500

    feat: calls for Defensive projected stats, Offensive current stats, & Player projected stats complete....

--- js/script.js
+++ js/script.js
@@ -1,11 +1,33 @@
 'use strict';
 
 
-const players = fetch(`https://api.sportsdata.io/api/nfl/fantasy/json/PlayerSeasonProjectionStats/2021REG?key=${SPORTS_API_TOKEN}`)
-       players.then(result => {
+//PLAYER *CURRENT* STATS FOR SEASON
+const playerCurrentStats = fetch(`https://api.sportsdata.io/api/nfl/fantasy/json/PlayerSeasonStats/2021REG?key=${SPORTS_API_TOKEN}`)
+    playerCurrentStats.then(result => {
+        result.json().then(data => {
+            console.log(data);
+        })
+    });
+
+
+
+//PLAYER SEASON *PROJECTED* STATS
+const playerSeasonProj = fetch(`https://api.sportsdata.io/api/nfl/fantasy/json/PlayerSeasonProjectionStats/2021REG?key=${SPORTS_API_TOKEN}`)
+       playerSeasonProj.then(result => {
            result.json().then(data => {
                console.log(data);
            })
        });
+
+//DEFENSIVE *PROJECTED* STATS
+const defSeasonProj = fetch(`https://api.sportsdata.io/api/nfl/fantasy/json/FantasyDefenseProjectionsBySeason/2021REG?key=${SPORTS_API_TOKEN}`)
+        defSeasonProj.then(result => {
+            result.json().then(data => {
+                console.log(data);
+            })
+        });
+
+
+
 
 
