6f9f2f204bc05214d091541cc796168cf3a17426
Anthony Villegas <villegasanthony87@yahoo.com>
2022-01-24 21:21:13 -0600

    feat: player entity created

--- src/main/java/com/warroom/leagueapi/model/Player.java
+++ src/main/java/com/warroom/leagueapi/model/Player.java
@@ -12,5 +12,13 @@ public class Player {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    private String first_name;
+
+    private String last_Name;
+
+    private String position;
+
+    private String college;
+
 
 }
